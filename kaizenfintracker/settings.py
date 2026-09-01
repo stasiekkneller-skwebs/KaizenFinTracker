efe9913eba32430e3c733ca615ca166fcf8073cc
Stasiek <stasiekkneller@gmail.com>
2026-09-01 23:12:16 +0200

    Dodałem warunki by nie renderowały się elementy jak nie ma transakcji

--- kaizenfintracker/settings.py
+++ kaizenfintracker/settings.py
@@ -121,6 +121,9 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 
 AUTH_USER_MODEL = 'users.User'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Email
 # https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
